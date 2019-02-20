325254573df57afcf29601cef33a100727b3880a
mmohamm9 <maryam.mohammadi@optum.com>
2019-02-20 12:19:03 -0800

    commit

---
+++ src/main/java/com/hemi/helloworld/service/controller/GreetingController.java
@@ -0,0 +1,22 @@
+package com.hemi.helloworld.service.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * Created by mmohamm9 on 2/20/2019.
+ */
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public String greeting() {
+        return "Hello  World!";
+    }
+}
